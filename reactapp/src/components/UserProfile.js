070c1e53af6c433539d6da2de94ac40e4a6a486f
Rishabh <rishabhgupta4523@gmail.com>
2023-03-21 03:28:34 +0530

    commit

--- reactapp/src/components/UserProfile.js
+++ reactapp/src/components/UserProfile.js
@@ -25,16 +25,32 @@ class UserProfile extends Component {
   render() {
     return (
       <div>
-        <div className="UserProfileCard">
-            <Stack direction="row" spacing={2} className="Card">
-                <Avatar 
-                alt="this.state.name" 
-                src="https://imgtr.ee/images/2023/03/20/qIWnl.jpg" 
-                sx={{ width: 46, height: 46 }}  
-                />
-                <p>{this.state.name} </p>
-            </Stack>
-            <p className='age'>{this.state.age} years old</p>
+        <div className="ProfileCard">
+          <div className="UserProfileCard">
+              <Stack direction="row" spacing={2} className="Card">
+                  <Avatar 
+                  alt="this.state.name" 
+                  src="https://imgtr.ee/images/2023/03/20/qIWnl.jpg" 
+                  sx={{ width: 46, height: 46 }}  
+                  />
+                  <p>{this.state.name} </p>
+              </Stack>
+              <p className='age'>{this.state.age} years old</p>
+              <div className='bottom_Card'>
+                <span className='height'>
+                  Height &nbsp; &nbsp; 
+                  {this.state.height} cm
+                </span>
+                <span className='weight'>
+                  Weight &nbsp; &nbsp; 
+                  {this.state.weight} kg
+                </span>
+                <span className='bloodType'>
+                  Blood Group 
+                  {this.state.bloodType} 
+                </span>
+              </div>
+          </div>
         </div>
       </div>
     );
