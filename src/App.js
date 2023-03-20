5f07736cd197de6979b5c317734befa2602cb536
Rishabh <rishabhgupta4523@gmail.com>
2023-03-20 23:53:54 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import {Button } from '@mui/material';
+import UserProfile from './components/UserProfile';
+
 
 function App() {
   return (
     <div>
-      <Button variant="contained" color="primary">Login</Button>
+      <UserProfile />
     </div>
   );
 }
---
+++ src/components/UserProfile.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import './UserProfile.css'
+
+
+class UserProfile extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: 'Rishabh Gupta',
+      age: '17',
+      gender: 'M',
+      location: 'Ghaziabad',
+      height: '175',
+      weight: '70',
+      bloodType: 'A+',
+      bloodSugar: '100',
+      bloodPressure: '130/80',
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="UserProfileCard">
+            <Stack direction="row" spacing={2} className="Card">
+                <Avatar 
+                alt="this.state.name" 
+                src="https://imgtr.ee/images/2023/03/20/qIWnl.jpg" 
+                sx={{ width: 46, height: 46 }}  
+                />
+                <p>{this.state.name} </p>
+            </Stack>
+            <p className='age'>{this.state.age} years old</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default UserProfile;
+
+  
