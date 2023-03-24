c2afdb3e6f4817fe0e83d65d69f16a6bfddc8c19
Rishabh <rishabhgupta4523@gmail.com>
2023-03-25 00:47:44 +0530

    commit

--- reactapp/src/components/UserProfile.js
+++ reactapp/src/components/UserProfile.js
@@ -1,74 +1,78 @@
-import React, { Component } from 'react';
-import Avatar from '@mui/material/Avatar';
-import Stack from '@mui/material/Stack';
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
-import './UserProfile.css'
-
+import React, { Component } from "react";
+import Avatar from "@mui/material/Avatar";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import "./UserProfile.css";
+import Button from "@mui/material/Button";
+import DeleteIcon from "@mui/icons-material/Delete";
+import Appointment from "./Appointments";
+import KeyboardBackspaceIcon from "@mui/icons-material/KeyboardBackspace";
 
 class UserProfile extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      name: 'Rishabh Gupta',
-      age: '17',
-      gender: 'M',
-      location: 'Ghaziabad',
-      height: '175',
-      weight: '60',
-      bloodType: 'A+',
-      bloodSugar: '100',
-      bloodPressure: '130/80',
-      countryCode : '+91',
-      phoneNumber:'9889069688',
-      email: 'rishabhgupta4523@gmail.com',
-      passportNumber: '1234567890',
-      medicalConditions: 'Cough, Cold'
+      name: "Rishabh Gupta",
+      age: "17",
+      gender: "M",
+      location: "Ghaziabad",
+      height: "175",
+      weight: "60",
+      bloodType: "A+",
+      bloodSugar: "100",
+      bloodPressure: "130/80",
+      countryCode: "+91",
+      phoneNumber: "9889069688",
+      email: "rishabhgupta4523@gmail.com",
+      passportNumber: "1234567890",
+      medicalConditions: "Cough, Cold",
     };
   }
 
   render() {
-    const Item = styled(Paper)(({ theme }) => ({
-      backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-      ...theme.typography.body2,
-      padding: theme.spacing(1),
-      textAlign: 'center',
-      color: theme.palette.text.secondary,
-    }));
     return (
       <div>
+        <div className="navBarProfile">
+          <div className="ButtonBack">
+            <Button variant="contained" startIcon={<KeyboardBackspaceIcon/>} size="small" id="backButton">
+              Back
+            </Button>
+            <span>Patient Profile</span>
+          </div>
+          <div className="AppointmentInfo">
+            <Appointment />
+          </div>
+        </div>
         <div className="ProfileCard">
           <div className="UserProfileCard">
-              <Stack direction="row" spacing={2} className="Card">
-                  <Avatar 
-                  alt="this.state.name" 
-                  src="https://imgtr.ee/images/2023/03/20/qIWnl.jpg" 
-                  sx={{ width: 46, height: 46 }}  
-                  />
-                  <p>{this.state.name} </p>
-              </Stack>
-              <p className='age'>{this.state.age} years old</p>
-              <div className='bottom_Card'>
-                <span className='height'>
-                  Height &nbsp; &nbsp; 
-                  {this.state.height}
-                </span>
-                <span className='weight'>
-                  Weight &nbsp; &nbsp; 
-                  {this.state.weight}
-                </span>
-                <span className='bloodType'>
-                  Blood Group &nbsp; &nbsp;
-                  {this.state.bloodType}
-                </span>
-              </div>
+            <Stack direction="row" spacing={2} className="Card">
+              <Avatar
+                alt="this.state.name"
+                src="https://imgtr.ee/images/2023/03/20/qIWnl.jpg"
+                sx={{ width: 46, height: 46 }}
+              />
+              <input hidden accept="image/*" type="file" />
+              <p>{this.state.name} </p>
+            </Stack>
+            <p className="age">{this.state.age} years old</p>
+            <div className="bottom_Card">
+              <span className="height">
+                Height &nbsp; &nbsp;
+                {this.state.height}
+              </span>
+              <span className="weight">
+                Weight &nbsp; &nbsp;
+                {this.state.weight}
+              </span>
+              <span className="bloodType">
+                Blood Group &nbsp; &nbsp;
+                {this.state.bloodType}
+              </span>
+            </div>
           </div>
           <div className="UserInfo">
-            <Box className="BoxCardInfo">
-            </Box>
+            <Box className="BoxCardInfo"></Box>
           </div>
         </div>
       </div>
@@ -77,5 +81,3 @@ class UserProfile extends Component {
 }
 
 export default UserProfile;
-
-  
